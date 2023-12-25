e3c193f80162c124651c9ad7209eef5f160d0e6c
Zanthyr <geyskens@pm.me>
2023-12-25 11:00:57 +0100

    partial refractor index.js

--- controllers/jobController.js
+++
@@ -1,9 +0,0 @@
-const catchAsync = require('../utils/catchAsync');
-const Job = require('./../models/jobModel');
-const factory = require('./handlerFactory');
-
-exports.getAllJobs = factory.getAll(Job);
-exports.getJob = factory.getOne(Job); // factory.getOne(Tour, { path: 'reviews' });
-exports.createJob = factory.createOne(Job);
-exports.updateJob = factory.updateOne(Job);
-exports.deleteJob = factory.deleteOne(Job);
