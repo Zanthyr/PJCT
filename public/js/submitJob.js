86369865a68e30e46cb19cfee59a2cd52a5d13a5
Zanthyr <geyskens@pm.me>
2024-01-11 10:35:31 +0100

    added submit page layout

--- public/js/submitJob.js
+++ public/js/submitJob.js
@@ -1,5 +1,17 @@
 import * as httpx from './httpx';
 
 export const submitJob = (domElement) => {
-  console.log('test', domElement);
+  const colors = JSON.parse(domElement.getAttribute('colors'));
+  // const id = domElement.getAttribute('artworkID');
+
+  colors.forEach((color, index) => {
+    const [x, y] = color.coords;
+    const marker = document.createElement('div');
+    marker.classList.add('marker');
+    marker.textContent = index + 1;
+    marker.style.position = 'absolute';
+    marker.style.left = `${x}px`;
+    marker.style.top = `${y}px`;
+    document.getElementById('markerImage').appendChild(marker);
+  });
 };
